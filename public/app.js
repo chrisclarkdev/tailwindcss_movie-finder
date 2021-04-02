ca747a5631e540b7800d2d4f87d457fdc02b249c
Chrisclrk9 <chrisclrk9@gmail.com>
2021-04-02 10:15:49 +0100

    overall
    Changed the  app to movie trailer app

--- public/app.js
+++ public/app.js
@@ -1,43 +1,47 @@
 btn = document.querySelector('#searchBtn');
+input = document.querySelector('#input');
 btn.addEventListener('click', getSearch);
+// btn.addEventListener('hover', refreshPage)
 
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
 function getSearch() {
-  search = document.querySelector('#input').value;
-  console.log(search)
+  const container = document.querySelector('.main')
+  removeAllChildNodes(container);
 
+
+  search = document.querySelector('#input').value;
+   
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=a876e7500012d962d40cf6ba7bd19019&language=en-US&query=${search}&page=1&include_adult=false`)
   .then((response) => {
     return response.json();
   }).catch((err) => alert(err, "I have no idea what's going on!!!"))
   .then((movies) => {
-    console.log(movies.results);
+    // console.log(movies.results);
  movieCard =  movies.results.map(movie => {
     return movie;
   });
-
-  // document.getElementById("movie-search").onfocus = function() {clearDOM()};
-  // function clearDOM() {
-  //   location.reload();
-
-  // }
   getmovieCard()
 });
 }
 function getmovieCard() {
   movieCard.forEach( movie => {
     let moviediv = document.createElement('div');
-    moviediv.classList.add('movies-container');
+    moviediv.classList.add('movie-container', 'hover-shadow', 'md:bg-white', 'md:w-72' ,'lg:w-full', 'lg:justify-center', 'rounded');
     
     let title = document.createElement('h1');
     let movieTitle = document.createTextNode(movie.title);
-    title.classList.add('movie-title', 'text-4xl', 'md:text-lg');
+    title.classList.add('movie-title_red', 'text-center' , 'text-4xl', 'md:text-lg');
     moviediv.appendChild(title);
-
 
     const imgurl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
     let img = document.createElement('img');
-    img.classList.add('shadow-huge', 'md:bg-white', 'md:w-72' ,'lg:w-full', 'lg:justify-center', 'rounded');
+    
     if (imgurl == "https://image.tmdb.org/t/p/w500/null"){
       img.src = "/public/images/coming-soon.jpg";
     } else {
@@ -45,12 +49,18 @@ function getmovieCard() {
     }
     moviediv.appendChild(img);
 
+    // ADD A FLEX DIV
+    let flexEl = document.createElement('div')
+    flexEl.classList.add('flex', 'justify-around', 'bg-grey-300','p-4')
+    moviediv.appendChild(flexEl)
+    // document.querySelector('.flex').appendChild(moviediv);
+
     let releaseDate = document.createTextNode(movie.release_date);
     let year = releaseDate.data.split('-');
     let yearRelease = document.createElement('p');
     yearRelease.classList.add('year');
-    yearRelease.append(`(${year[0]}) `);
-    moviediv.appendChild(yearRelease);
+    yearRelease.append(`Year: (${year[0]}) `);
+    flexEl.appendChild(yearRelease);
     document.querySelector('.main').appendChild(moviediv);
 
     // rating 
@@ -58,13 +68,14 @@ function getmovieCard() {
     rating_paragragh = document.createElement('p');
     i_class = document.createElement('i');
     rating_paragragh.classList.add('ratings');
-    i_class.classList.add("fas");
-    i_class.classList.add("fa-star");
+    i_class.classList.add("fa",'fa-star','py-1');
+   
     rating_paragragh.appendChild(i_class);
     i_class.appendChild(rating);
-    moviediv.appendChild(rating_paragragh);
+    flexEl.appendChild(rating_paragragh);
     document.querySelector('.main').appendChild(moviediv);
-  
+
+
     // add a href to movie title 
 
     let linkTag = document.createElement('a');
@@ -76,27 +87,82 @@ function getmovieCard() {
 
     linkTag.innerHTML = movieTitle.data.length < 22 ? `${movieTitle.data}` : `${movieTitle.data.fontsize(4.5)}`;
     // linkTag.innerHTML = movieTitle.data;
-    linkTag.addEventListener('click', function() {movieSelected(movie)});
+    moviediv.addEventListener('click', ()=> {movieSelected(movie)});
+    
+    moviediv.onclick = function() {
+      modal.style.display = "block";
+      getVideo(movie)
+    }
     document.querySelector('.main').appendChild(moviediv);
 
   
    // disable button //
-    btn = document.querySelector('#searchBtn');
-    if(moviediv.innerHTML !== ""){
-      btn.setAttribute('disabled', true);
-    } else {
-      btn.setAttribute('disabled', false)
-    }
+    // btn = document.querySelector('#searchBtn');
+    // if(moviediv.innerHTML !== ""){
+    //   btn.setAttribute('disabled', true);
+    // } else {
+    //   btn.setAttribute('disabled', false)
+    // }
+  })
+ 
+}
 
+let frame = document.createElement('IFRAME');
+function getVideo(movie) {
+  const videoUrl = `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=a876e7500012d962d40cf6ba7bd19019&language=en-US`;
+  fetch(videoUrl)
+  .then((response) => {
+    return response.json();
+  }).catch((err) => alert(err))
+  .then((video) => {
+    const videoId = video.results[0].key;
+    
+    if (videoId == undefined){
+      
+      console.log("no video")
+      document.body.appendChild(modal).innerHTML = "No Video Available";
+    } else {
+    console.log(videoId)
+    const youtube = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&mute=1";
+    const video_container = document.createElement('div');
+    video_container.classList.add('youtube', 'mx-auto' ,'px-4', 'flex', 'justify-center' )
+    youtubeWidth = window.innerWidth;
+    if (innerWidth > 1000 ){
+     frame.setAttribute('width', youtubeWidth/2 +'px');
+     frame.setAttribute('height', youtubeWidth/3 +'px');
+    }else {
+     frame.setAttribute('width', `${youtubeWidth - 38}px`);
+     frame.setAttribute('height', youtubeWidth/1.77777 +'px');
+    }
+    
+    frame.src = youtube;
+    video_container.appendChild(frame);
+    content = document.querySelector('.modal-content')
+    content.appendChild(video_container)
+    document.body.appendChild(modal);
+  }
   })
 }
+ let modal = document.getElementById("myModal");
+let videoCon = document.querySelector('.modal-content')
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
 
- // end of function
-
- function movieSelected(id) {
-  //  console.log(id.id)
-  sessionStorage.setItem("movieId", id.id);
-  console.log(id.id);
-  window.location = "movie_page.html";
-  return id;
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  frame.remove()
 }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// function movieSelected(movie) {
+ 
+// }
+
+
+ 
